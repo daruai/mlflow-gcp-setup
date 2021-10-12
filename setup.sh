8a9a6abd5f35f4981ed113f6aec09686811d8503
Daniel Ruiz <danielruizriquelme@gmail.com>
2021-10-12 15:16:31 +0200

    fix: adding random id to bucket and sql to avoid name collisions

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 CLOUD_SQL_PASS=$1
 
-export PROJECT_ID=$(gcloud config get-value project)
+PROJECT_ID=$(gcloud config get-value project)
+RANDOM_ID=$(od -x /dev/urandom | head -1 | awk '{OFS="-"; print $2}')
 
 # Building mlflow image
 docker build -t mlflow:1.14.1 .
@@ -9,28 +10,29 @@ docker build -t mlflow:1.14.1 .
 gcloud iam service-accounts create mlflow-tracking-sa --description="Service Account to run the MLFLow tracking server" --display-name="MLFlow tracking SA"
 
 # Bucket
-gsutil mb gs://$PROJECT_ID-mlflow-bucket
+BUCKET_NAME=gs://$PROJECT_ID-mlflow-$RANDOM_ID
+gsutil mb $BUCKET_NAME
 
 # Cloud SQL
-
-gcloud sql instances create mlflow-backend --tier=db-f1-micro --region=us-central1 --root-password=$CLOUD_SQL_PASS --storage-type=SSD --async
-PENDING_OPERATIONS=$(gcloud sql operations list --instance=mlflow-backend --filter='status!=DONE' --format='value(name)')
+SQL_NAME=mlflow-backend-$RANDOM_ID
+gcloud sql instances create $SQL_NAME --tier=db-f1-micro --region=us-central1 --root-password=$CLOUD_SQL_PASS --storage-type=SSD --async
+PENDING_OPERATIONS=$(gcloud sql operations list --instance=$SQL_NAME --filter='status!=DONE' --format='value(name)')
 gcloud sql operations wait "${PENDING_OPERATIONS}" --timeout=unlimited
 
 # Service account permissions
-gsutil iam ch 'serviceAccount:mlflow-tracking-sa@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin' gs://$PROJECT_ID-mlflow-bucket
+gsutil iam ch 'serviceAccount:mlflow-tracking-sa@$PROJECT_ID.iam.gserviceaccount.com:roles/storage.admin' $BUCKET_NAME
 gcloud project add-iam-policy-binding $PROJECT_ID --member='serviceAccount:mlflow-tracking-sa@$PROJECT_ID.iam.gserviceaccount.com' --role=roles/cloudsql.editor
 
 # Filling placeholders in startup script
 sed 's/<PROJECT-ID>/'$PROJECT_ID'/' start_mlflow_tracking.sh > start_mlflow_tracking.sh
-sed 's/<BUCKET-NAME>/gs://'$PROJECT_ID'-mlflow-bucket/' start_mlflow_tracking.sh > start_mlflow_tracking.sh
+sed 's/<BUCKET-NAME>/'$BUCKET_NAME'/' start_mlflow_tracking.sh > start_mlflow_tracking.sh
 sed 's/<YOUR_ROOT_PASSWORD>/'$CLOUD_SQL_PASS'/' start_mlflow_tracking.sh > start_mlflow_tracking.sh
-
+sed 's/<SQL-NAME>/'$SQL_NAME'/' start_mlflow_tracking.sh > start_mlflow_tracking.sh
 # Moving startup script to bucket
-gsutil cp start_mlflow_tracking.sh gs://$PROJECT_ID-mlflow-bucket/scripts/start_mlflow_tracking.sh
+gsutil cp start_mlflow_tracking.sh $BUCKET_NAME/scripts/start_mlflow_tracking.sh
 
 # Compute engine
-gcloud beta compute --project=$PROJECT_ID instances create mlflow-tracking-server --zone=us-central1-a --machine-type=e2-medium --subnet=default --network-tier=PREMIUM --metadata=startup-script-url=gs://$PROJECT_ID-mlflow-bucket/scripts/start_mlflow_tracking.sh --maintenance-policy=MIGRATE --service-account=mlflow-tracking-sa@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=mlflow-tracking-server --image=cos-77-12371-1109-0 --image-project=cos-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=mlflow-tracking-server --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
+gcloud beta compute --project=$PROJECT_ID instances create mlflow-tracking-server --zone=us-central1-a --machine-type=e2-medium --subnet=default --network-tier=PREMIUM --metadata=startup-script-url=$BUCKET_NAME/scripts/start_mlflow_tracking.sh --maintenance-policy=MIGRATE --service-account=mlflow-tracking-sa@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=mlflow-tracking-server --image=cos-77-12371-1109-0 --image-project=cos-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=mlflow-tracking-server --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 
 # Firewall
 gcloud compute firewall-rules create allow-mlflow-tracking --network default --priority 1000 --direction ingress --action allow --target-tags mlflow-tracking-server --source-ranges 0.0.0.0/0 --rules tcp:5000 --enable-logging
